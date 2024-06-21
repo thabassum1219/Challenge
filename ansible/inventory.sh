f2b1282e9a3916de374f28d3fce1bbbd43c779c0
thabassum1219 <thabassum1219 @gmail.com>
2024-06-21 11:33:12 +0000

    Add Terraform and Ansible configurations

--- ansible/inventory.sh
+++ ansible/inventory.sh
@@ -1,5 +1,20 @@
 #[webservers]
 #172.31.4.72
+##!/bin/bash
+#terraform output -raw inventory > ansible/inventory
 #!/bin/bash
-terraform output -raw inventory > ansible/inventory
+cat << EOF
+{
+  "frontend": {
+    "hosts": [
+      "$(terraform output c8_public_ip)"
+    ]
+  },
+  "backend": {
+    "hosts": [
+      "$(terraform output u21_public_ip)"
+    ]
+  }
+}
+EOF
 
