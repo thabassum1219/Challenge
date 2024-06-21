5250dd500609876809f2224d8a45507447a1e24b
thabassum1219 <thabassum1219 @gmail.com>
2024-06-21 11:26:27 +0000

    Add Terraform and Ansible configurations

---
+++ terraform/main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+# Generate a new SSH key pair
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+# Create a key pair in AWS
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = tls_private_key.example.public_key_openssh
+}
+
+resource "aws_instance" "amazon_linux" {
+  ami           = "ami-033fabdd332044f06"  # Amazon Linux 2 AMI for us-east-2
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.deployer.key_name
+  tags = {
+    Name = "c8.local"
+  }
+}
+
+resource "aws_instance" "ubuntu" {
+  ami           = "ami-09040d770ffe2224f"  # Ubuntu 21.04 AMI for us-east-2
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.deployer.key_name
+  tags = {
+    Name = "u21.local"
+  }
+}
+
+output "private_key_pem" {
+  value     = tls_private_key.example.private_key_pem
+  sensitive = true
+}
+
+output "inventory" {
+  value = <<EOF
+[frontend]
+c8.local ansible_host=${aws_instance.amazon_linux.public_ip}
+
+[backend]
+u21.local ansible_host=${aws_instance.ubuntu.public_ip}
+EOF
+}
+
